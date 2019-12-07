ef75254b43882d71e42900003983d52ee881a6a0
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2019-12-07 17:22:01 +0100

    cleaned up main program

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/labstack/echo"
@@ -13,11 +14,64 @@ import (
 )
 
 var production bool
+var h *handler.Handler
+var e *echo.Echo
+var db *gorm.DB
+var err error
 
 func main() {
 	loadEnvironment()
+	initializeEcho()
+	initializeDB()
+	h = &handler.Handler{DB: db}
+	registerRoutes()
 
-	e := echo.New()
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func registerRoutes() {
+	e.GET("/", h.MainRoute)
+	e.GET("/users", h.GetUsers)
+	e.GET("/users/:id", h.GetUsersById)
+	e.POST("/users", h.CreateUser)
+	e.PUT("/users/:id", h.UpdateUser)
+	e.DELETE("/users/:id", h.DeleteUser)
+	e.PUT("/settings/:id", h.UpdateUserCosmosSettings)
+	e.GET("/settings/:id", h.GetCosmosSettings)
+	e.POST("/login", h.Login)
+	e.POST("/register", h.Register)
+}
+
+func initializeDB() {
+	if production {
+		fmt.Printf("--- PRODUCTION MODE ACTIVE ---")
+		db, err = gorm.Open("postgres", "host="+os.Getenv("PG_ADDRESS")+" port="+os.Getenv("PG_PORT")+" user="+os.Getenv("PG_USER")+" dbname="+os.Getenv("PG_DB")+" password="+os.Getenv("PG_PASSWORD")+" sslmode=disable")
+	} else {
+		fmt.Printf("--- DEVELOPMENT MODE ACTIVE ---")
+		db, err = gorm.Open("sqlite3", "main.db")
+	}
+
+	if err != nil {
+		print(err.Error())
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&types.User{})
+	db.AutoMigrate(&types.CosmosUser{})
+}
+
+func loadEnvironment() {
+	if os.Getenv("ENVIRONMENT") == "production" {
+		production = true
+	} else {
+		production = false
+	}
+}
+
+func initializeEcho() {
+	e = echo.New()
 	e.Logger.SetLevel(log.ERROR)
 	e.Use(middleware.Logger())
 	e.Use(middleware.JWTWithConfig(middleware.JWTConfig{
@@ -31,47 +85,4 @@ func main() {
 		},
 	}))
 	e.Use(middleware.Recover())
-
-	var db *gorm.DB
-	var err error
-	if production {
-		log.Printf("--- PRODUCTION MODE ACTIVE ---")
-		db, err = gorm.Open("postgres", "host="+os.Getenv("PG_ADDRESS")+" port="+os.Getenv("PG_PORT")+" user="+os.Getenv("PG_USER")+" dbname="+os.Getenv("PG_DB")+" password="+os.Getenv("PG_PASSWORD")+" sslmode=disable")
-	} else {
-		log.Printf("--- DEVELOPMENT MODE ACTIVE ---")
-		db, err = gorm.Open("sqlite3", "main.db")
-	}
-
-	if err != nil {
-		print(err.Error())
-		panic("failed to connect database")
-	}
-	defer db.Close()
-
-	// Migrate the schema
-	db.AutoMigrate(&types.User{})
-	db.AutoMigrate(&types.CosmosUser{})
-
-	h := &handler.Handler{DB: db}
-
-	e.GET("/", h.MainRoute)
-	e.GET("/users", h.GetUsers)
-	e.GET("/users/:id", h.GetUsersById)
-	e.POST("/users", h.CreateUser)
-	e.PUT("/users/:id", h.UpdateUser)
-	e.DELETE("/users/:id", h.DeleteUser)
-	e.PUT("/settings/:id", h.UpdateUserCosmosSettings)
-	e.GET("/settings/:id", h.GetCosmosSettings)
-	e.POST("/login", h.Login)
-	e.POST("/register", h.Register)
-
-	e.Logger.Fatal(e.Start(":1323"))
-}
-
-func loadEnvironment() {
-	if os.Getenv("ENVIRONMENT") == "production" {
-		production = true
-	} else {
-		production = false
-	}
 }
